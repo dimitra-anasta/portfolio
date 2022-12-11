293bdd48df5f646d7fbb11f92d681ca9d6c5506f
Dimitra Anastasooulos <dimitra.anasta524@gmail.com>
2022-12-11 12:55:15 -0500

    padding and links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className=''>
-    <div name='contact' className='w-full h-screen  bg-[#0a192f] flex justify-center items-center p-4'>
+    <div className='bg-[#0a192f]'>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <form method ='POST' action="https://getform.io/f/3d00c629-5a52-4033-b610-b0bb0d852ec1" className='items center flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-[#a66baf] text-gray-300'>Contact</p>
+                <p className='text-4xl pt-8 font-bold inline border-b-4 border-[#a66baf] text-gray-300'>Contact</p>
                 <p  className='text-gray-300 py-4'>Submit the form below or send me an email - dimitra.anasta524@gmail.com</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
